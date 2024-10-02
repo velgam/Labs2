a124a49b4d0fa0d05e818a1002e089cf21201fbb
Токарский Илья <ilyhatokarskiy228@gmail.com>
2024-10-02 17:23:21 +0700

    Информация об авторе

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def start():
+@app.route ("/")
+@app.route ("/web")
+def web():
     return """<!doctype.html> 
         <html> 
                <body> 
                       <h1>web-сервер на flask</h1> 
+                       <a href = "/author">author</a>
                </body> 
         </html>"""
 
+@app.route("/author")
+def author():
+    name = "Токарский Илья Андреевич"
+    group = "ФБИ-22"
+    faculty = "ФБ"
+
+    return """<!doctype.html> 
+        <html> 
+               <body> 
+                      <p>Студент: """ + name + """</p>
+                      <p>Группа: """ + group + """</p>
+                      <p>Факультет: """ + faculty + """</p>
+                      <a href = "/web">web</a>
+               </body> 
+        </html>"""

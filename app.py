12bdf8f85c91d12e6be3aeac4d6d376352a7d91b
velgam <ilyhatokarskiy228@gmail.com>
2024-10-16 03:44:04 +0700

    Коды ответов HTTP

--- app.py
+++ app.py
@@ -4,7 +4,20 @@ app = Flask(__name__)
 
 @app.errorhandler(404)
 def not_found(err):
-       return "нет такой страницы", 404
+    path = url_for("static", filename = "404.png")
+    style = url_for("static", filename = "lab1.css")
+    return '''
+<!doctype html>
+<html>
+<head>
+    <link rel = "stylesheet" href="''' + style +'''"
+</head>
+    <body>
+        <img src="''' + path + '''" class="full-screen-image">
+    </body>
+</html>
+''', 404
+
 @app.route('/')
 @app.route('/index')
 def index():
@@ -21,68 +34,82 @@ def index():
            <body>
                 <a href='/lab1'>Первая лабораторная</a>
            </body>
-           <footer>Токарский Илья Андреевич, ФБИ-22, 3 курс, 2024</footer>
+           <footer>Иванов Егор Владиславович, ФБИ-22, 3 курс, 2024</footer>
         </html>''', 200
 
-@app.route('/lab1')
-def lab1():
-    style = url_for("static", filename = "lab1.css")
+    @app.route('/lab1')
+    def lab1():
+        style = url_for("static", filename = "lab1.css")
+        return '''<!doctype html>
+            <html>
+            <head>
+                <link rel = "stylesheet" href="''' + style +'''"
+                <title>Лабораторная 1</title>
+            </head>
+            <body>
+                    <p>
+                        Flask — фреймворк для создания веб-приложений на языке
+                        программирования Python, использующий набор инструментов
+                        Werkzeug, а также шаблонизатор Jinja2. Относится к категории так
+                        называемых микрофреймворков — минималистичных каркасов
+                        веб-приложений, сознательно предоставляющих лишь самые базовые возможности.
+                    </p>
+                    <a href="/">Главная страница</a>
+                    <h2>Список роутов</h2>
+                    <ul>
+                        <li><a href="/">Главная страница</a></li>
+                        <li><a href="/index">Главная страница (index)</a></li>
+                        <li><a href="/lab1">Первая лабораторная</a></li>
+                        <li><a href="/lab1/web">Web</a></li>
+                        <li><a href="/lab1/author">Автор</a></li>
+                        <li><a href="/lab1/oak">Дуб</a></li>
+                        <li><a href="/lab1/counter">Счетчик</a></li>
+                        <li><a href="/lab1/reset_counter">Сброс счетчика</a></li>
+                        <li><a href="/lab1/info">Информация</a></li>
+                        <li><a href="/lab1/created">Создано успешно</a></li>
+                        <li><a href="/error/400">Ошибка 400</a></li>
+                        <li><a href="/error/401">Ошибка 401</a></li>
+                        <li><a href="/error/402">Ошибка 402</a></li>
+                        <li><a href="/error/403">Ошибка 403</a></li>
+                        <li><a href="/error/405">Ошибка 405</a></li>
+                        <li><a href="/error/418">Ошибка 418</a></li>
+                        <li><a href="/trigger_error">Триггер ошибки</a></li>
+                        <li><a href="/heavy_metal">Тяжелый металл</a></li>
+                    </ul>
+            </body>
+            <footer>Иванов Егор Владиславович, ФБИ-22, 3 курс, 2024</footer>
+            </html>''', 200
+
+@app.route('/lab1/web')
+def start():
     return '''<!doctype html>
         <html>
-        <head>
-            <link rel = "stylesheet" href="''' + style +'''"
-            <title>Лабораторная 1</title>
-        </head>
            <body>
-                <p>
-                    Flask — фреймворк для создания веб-приложений на языке
-                    программирования Python, использующий набор инструментов
-                    Werkzeug, а также шаблонизатор Jinja2. Относится к категории так
-                    называемых микрофреймворков — минималистичных каркасов
-                    веб-приложений, сознательно предоставляющих лишь самые базовые возможности.
-                </p>
-                <a href="/">Главная страница</a>
-                <h2>Список роутов</h2>
-                <ul>
-                    <li><a href="/">Главная страница</a></li>
-                    <li><a href="/index">Главная страница (index)</a></li>
-                    <li><a href="/lab1">Первая лабораторная</a></li>
-                    <li><a href="/lab1/web">Web</a></li>
-                    <li><a href="/lab1/author">Автор</a></li>
-                    <li><a href="/lab1/oak">Дуб</a></li>
-                    <li><a href="/lab1/counter">Счетчик</a></li>
-                    <li><a href="/lab1/cancel_counter">Сброс счетчика</a></li>
-                    <li><a href="/lab1/info">Информация</a></li>
-                    <li><a href="/lab1/created">Создано успешно</a></li>
-                    <li><a href="/error/400">Ошибка 400</a></li>
-                    <li><a href="/error/401">Ошибка 401</a></li>
-                    <li><a href="/error/402">Ошибка 402</a></li>
-                    <li><a href="/error/403">Ошибка 403</a></li>
-                    <li><a href="/error/405">Ошибка 405</a></li>
-                    <li><a href="/error/418">Ошибка 418</a></li>
-                    <li><a href="/trigger_error">Триггер ошибки</a></li>
-                    <li><a href="/heavy_metal">Тяжелый металл</a></li>
-                </ul>
+                <h1>web-сервер на flask</h1>
+                <a href='author'>author</a>  
+                <a href='/lab1/oak'>дуб</a> 
            </body>
-           <footer>Токарский Илья Андреевич, ФБИ-22, 3 курс, 2024</footer>
-        </html>''', 200
+        </html>''', 200, {
+            'X-Server,': 'sample',
+            'Content-type': 'text/plain; charset=utf-8'
+                          }
 
-
-@app.route("/lab1/author")
+@app.route('/lab1/author')
 def author():
-    name = "Токарский Илья Андреевич"
-    group = "ФБИ-22"
-    faculty = "ФБ"
+    name = 'Иванов Егор Владиславович'
+    group = 'ФБИ-22'
+    faculty = 'ФБ'
 
-    return """<!doctype.html> 
-        <html> 
-               <body> 
-                      <p>Студент: """ + name + """</p>
-                      <p>Группа: """ + group + """</p>
-                      <p>Факультет: """ + faculty + """</p>
-                      <a href = "/web">web</a>
-               </body> 
-        </html>"""
+    return '''<!doctype html>
+        <html>
+           <body>
+                <p>Студент: ''' + name + '''</p>
+                <p>Группа: ''' + group + '''</p>
+                <p>Факультет: ''' + faculty + '''</p>
+                <a href='web'>web</a>
+                <a href='/lab1/oak'>дуб</a>
+           </body>
+        </html>'''
 
 @app.route('/lab1/oak')
 def oak():
@@ -93,7 +120,7 @@ def oak():
 <html>
     <link rel = "stylesheet" href="''' + style +'''"
     <body>
-        <h1>ну дуб хихи</h1>
+        <h1>Дуб необходимо сажать на хорошо освещенном месте. Лучше избегать тяжелых глинистых почв или песчаных участков. Важно учесть размеры взрослого дуба и предусмотреть пространство его развития. Рекомендуемое расстояние от других деревьев или строений – 10-15 метров.</h1>
         <img src="''' + path + '''" class="oak-image">
     </body>
 </html>
@@ -105,7 +132,7 @@ count = 0
 def counter():
     global count
     count += 1
-    reset_link = url_for('cancel_counter')
+    reset_link = url_for('reset_counter')
     return '''
 <!doctype html>
 <html>
@@ -117,9 +144,15 @@ def counter():
 </html>
 '''
 
-@app.route('lab1/info')
+@app.route('/lab1/reset_counter')
+def reset_counter():
+    global count
+    count = 0
+    return redirect(url_for('counter'))
+
+@app.route('/lab1/info')
 def info():
-    return redirect('/author')
+    return redirect('/lab1/author')
 
 @app.route('/lab1/created')
 def created():
@@ -133,8 +166,90 @@ def created():
 </html>
 ''', 201
 
-@app.route('/lab1/cancel_counter')
-def cancel_counter():
-    global count
-    count = 0
-    return redirect(url_for('counter'))
+@app.route('/error/400')
+def error_400():
+    return 'Bad Request', 400
+
+@app.route('/error/401')
+def error_401():
+    return 'Unauthorized', 401
+
+@app.route('/error/402')
+def error_402():
+    return 'Payment Required', 402
+
+@app.route('/error/403')
+def error_403():
+    return 'Forbidden', 403
+
+@app.route('/error/405')
+def error_405():
+    return 'Method Not Allowed', 405
+
+@app.route('/error/418')
+def error_418():
+    return "I'm a teapot", 418
+
+@app.route('/trigger_error')
+def trigger_error():
+    # Вызываем ошибку деления на ноль
+    return 1 / 0
+
+@app.errorhandler(500)
+def internal_error(error):
+    return '''
+<!doctype html>
+<html>
+    <head>
+        <title>Ошибка сервера</title>
+    </head>
+    <body>
+        <h1>Произошла ошибка на сервере</h1>
+        <p>Пожалуйста, попробуйте позже.</p>
+    </body>
+</html>
+''', 500
+
+@app.route('/heavy_metal')
+def heavy_metal():
+    path = url_for("static", filename = "heavymetal.jpg")
+    style = url_for("static", filename = "lab1.css")
+    return '''
+<!doctype html>
+<html>
+    <head>
+        <link rel = "stylesheet" href="''' + style +'''"
+        <title>Альбом: Heavy Metal 2</title>
+        <style>
+            body {
+                background-color: black;
+                color: white;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Da Vinci</h1>
+        <p>
+            Столько tooties, tooties, tooties, нет…
+        </p>
+        <p>
+            Столько-столько tooties, tooties, tooties, нет… 
+        </p>
+        <p>
+            Столько tooties, столько tooties, tooties, tooties, нет
+            Я их не запомню
+        </p>
+        <p>
+            Столько tooties, столько tooties, tooties, tooties, нет
+        </p>
+        <img src="''' + path + '''" alt="Heavy Metal">
+    </body>
+</html>
+''', 200, {
+    'Content-Language': 'ru',
+    'X-Custom-Header-1': 'HeavyMetal2',
+    'X-Custom-Header-2': 'RR'
+}
+
+if __name__ == '__main__':
+    app.run(debug=True)

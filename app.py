bee64b4541ba4c091c928d9e0c4d390d8ffef9f8
velgam <ilyhatokarskiy228@gmail.com>
2024-10-16 04:05:02 +0700

    Беллочка

--- app.py
+++ app.py
@@ -151,3 +151,47 @@ def cancel_counter():
     global count
     count = 0
     return redirect(url_for('counter'))
+
+    @app.route('/error/400')
+def error_400():
+    return 'Bad Request', 400
+
+@app.route('/error/401')
+def error_401():
+    return 'Unauthorized', 401
+
+@app.route('/error/402')
+def error_402():
+    return 'Payment Required', 402
+
+@app.route('/error/403')
+def error_403():
+    return 'Forbidden', 403
+
+@app.route('/error/405')
+def error_405():
+    return 'Method Not Allowed', 405
+
+@app.route('/error/418')
+def error_418():
+    return "I'm a teapot", 418
+
+@app.route('/trigger_error')
+def trigger_error():
+    # Вызываем ошибку деления на ноль
+    return 1 / 0
+
+@app.errorhandler(500)
+def internal_error(error):
+    return '''
+<!doctype html>
+<html>
+    <head>
+        <title>Ошибка сервера</title>
+    </head>
+    <body>
+        <h1>Произошла ошибка на сервере</h1>
+        <p>Пожалуйста, попробуйте позже.</p>
+    </body>
+</html>
+''', 500

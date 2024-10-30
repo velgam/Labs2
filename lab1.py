a46e72585e56288b80b500000cd84dacde412e3f
Токарский Илья <ilyhatokarskiy228@gmail.com>
2024-10-30 16:56:18 +0700

    Рефакторинг

---
+++ lab1.py
@@ -0,0 +1,171 @@
+from flask import Blueprint, url_for, redirect
+lab1 = Blueprint('lab1', __name__)
+
+
+@lab1.route('/lab1')
+def lab():
+    style = url_for("static", filename = "lab1.css")
+    return '''<!doctype html>
+        <html>
+        <head>
+            <link rel = "stylesheet" href="''' + style +'''"
+            <title>Лабораторная 1</title>
+        </head>
+        <body>
+                <p>
+                    Flask — фреймворк для создания веб-приложений на языке
+                    программирования Python, использующий набор инструментов
+                    Werkzeug, а также шаблонизатор Jinja2. Относится к категории так
+                    называемых микрофреймворков — минималистичных каркасов
+                    веб-приложений, сознательно предоставляющих лишь самые базовые возможности.
+                </p>
+                <a href="/">Главная страница</a>
+                <h2>Список роутов</h2>
+                <ul>
+                    <li><a href="/">Главная страница</a></li>
+                    <li><a href="/index">Главная страница (index)</a></li>
+                    <li><a href="/lab1">Первая лабораторная</a></li>
+                    <li><a href="/lab1/web">Web</a></li>
+                    <li><a href="/lab1/author">Автор</a></li>
+                    <li><a href="/lab1/oak">Дуб</a></li>
+                    <li><a href="/lab1/counter">Счетчик</a></li>
+                    <li><a href="/lab1/cancel_counter">Сброс счетчика</a></li>
+                    <li><a href="/lab1/info">Информация</a></li>
+                    <li><a href="/lab1/created">Создано успешно</a></li>
+                    <li><a href="/error/400">Ошибка 400</a></li>
+                    <li><a href="/error/401">Ошибка 401</a></li>
+                    <li><a href="/error/402">Ошибка 402</a></li>
+                    <li><a href="/error/403">Ошибка 403</a></li>
+                    <li><a href="/error/405">Ошибка 405</a></li>
+                    <li><a href="/error/418">Ошибка 418</a></li>
+                    <li><a href="/trigger_error">Триггер ошибки</a></li>
+                    <li><a href="/model">Белла Хадид</a></li>
+                </ul>
+        </body>
+        <footer>Токарский Илья Андреевич, ФБИ-22, 3 курс, 2024</footer>
+        </html>''', 200
+
+
+@lab1.route('/lab1/web')
+def start():
+    return '''<!doctype html>
+        <html>
+           <body>
+                <h1>web-сервер на flask</h1>
+                <a href='author'>author</a>  
+                <a href='/lab1/oak'>дуб</a> 
+           </body>
+        </html>''', 200, {
+            'X-Server,': 'sample',
+            'Content-type': 'text/plain; charset=utf-8'
+                          }
+
+
+@lab1.route('/lab1/author')
+def author():
+    name = 'Токарский Илья Андреевич'
+    group = 'ФБИ-22'
+    faculty = 'ФБ'
+
+    return '''<!doctype html>
+        <html>
+           <body>
+                <p>Студент: ''' + name + '''</p>
+                <p>Группа: ''' + group + '''</p>
+                <p>Факультет: ''' + faculty + '''</p>
+                <a href='web'>web</a>
+                <a href='/lab1/oak'>дуб</a>
+           </body>
+        </html>'''
+
+
+@lab1.route('/lab1/oak')
+def oak():
+    path = url_for("static", filename = "oak.jpg")
+    style = url_for("static", filename = "lab1.css")
+    return '''
+<!doctype html>
+<html>
+    <link rel = "stylesheet" href="''' + style +'''"
+    <body>
+        <h1>дубик хихи)</h1>
+        <img src="''' + path + '''" class="oak-image">
+    </body>
+</html>
+'''
+
+count = 0
+
+
+@lab1.route('/lab1/counter')
+def counter():
+    global count
+    count += 1
+    reset_link = url_for('cancel_counter')
+    return '''
+<!doctype html>
+<html>
+    <body>
+        Сколько раз вы сюда заходили: ''' + str(count) + '''
+        <br>
+        <a href="''' + reset_link + '''">Очистить счётчик</a>
+    </body>
+</html>
+'''
+
+
+@lab1.route('/lab1/cancel_counter')
+def cancel_counter():
+    global count
+    count = 0
+    return redirect(url_for('counter'))
+
+
+@lab1.route('/lab1/info')
+def info():
+    return redirect('/lab1/author')
+
+
+@lab1.route('/lab1/created')
+def created():
+    return '''
+<!doctype html>
+<html>
+    <body>
+        <h1>Создано успешно</h1>
+        <div><i>что-то создано...</i></div>
+    </body>
+</html>
+''', 201
+
+
+@lab1.route('/lab1/model')
+def heavy_metal():
+    path = url_for("static", filename = "Bella.jpg")
+    style = url_for("static", filename = "lab1.css")
+    return '''
+<!doctype html>
+<html>
+    <head>
+        <link rel = "stylesheet" href="''' + style +'''"
+        <title>BELLA HADID</title>
+        <style>
+            body {
+                background-color: black;
+                color: white;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Bella</h1>
+        <p>
+           маделирую шмаделирую чиста э уа афап
+        </p>
+        <img src="''' + path + '''" alt="Bella">
+    </body>
+</html>
+''', 200, {
+    'Content-Language': 'ru',
+    'X-Custom-Header-1': 'bella',
+    'X-Custom-Header-2': 'bella'
+}

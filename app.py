91691b0d2d7f3d88b2c67e2e1489beb5207ee16d
velgam <ilyhatokarskiy228@gmail.com>
2024-12-26 17:29:13 +0700

    правки

--- app.py
+++ app.py
@@ -9,8 +9,48 @@ from lab8 import lab8
 from lab7 import lab7
 from lab9 import lab9
 from rgz import rgz
+from os import path
+import os
+from flask_login import LoginManager
+from db import db
 
 app = Flask(__name__)
+
+# Инициализация LoginManager
+login_manager = LoginManager()
+login_manager.login_view = 'lab8.login'
+login_manager.init_app(app)
+
+# Настройка конфигурации приложения
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'Секретно-секретный секрет')
+app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
+
+# Настройка базы данных
+if app.config['DB_TYPE'] == 'postgres':
+    db_name = 'tok'
+    db_user = 'tok'
+    db_password = '123'
+    host_ip = '127.0.0.1'
+    host_port = 5432
+    app.config['SQLALCHEMY_DATABASE_URI'] = \
+        f'postgresql://{db_user}:{db_password}@{host_ip}:{host_port}/{db_name}'
+else:
+    dir_path = path.dirname(path.realpath(__file__))
+    db_path = path.join(dir_path, "database.db")
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+
+# Инициализация SQLAlchemy
+db.init_app(app)
+
+# Импорт моделей после инициализации db
+from db.models import users
+
+# Загрузчик пользователей для Flask-Login
+@login_manager.user_loader
+def load_users(login_id):
+    return users.query.get(int(login_id))
+
+# Регистрация Blueprint
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
@@ -22,32 +62,31 @@ app.register_blueprint(lab8)
 app.register_blueprint(lab9)
 app.register_blueprint(rgz)
 
-app.config['DB_TYPE'] = 'postgres'
-app.secret_key = ')))'
+# Обработчики ошибок
 @app.errorhandler(404)
 def not_found(err):
-    path = url_for("static", filename = "404.jpg")
-    style = url_for("static", filename = "lab1.css")
+    path = url_for("static", filename="404.jpg")
+    style = url_for("static", filename="lab1.css")
     return '''
 <!doctype html>
 <html>
 <head>
-    <link rel = "stylesheet" href="''' + style +'''"
+    <link rel="stylesheet" href="''' + style + '''">
 </head>
-    <body>
-        <img src="''' + path + '''" class="full-screen-image">
-    </body>
+<body>
+    <img src="''' + path + '''" class="full-screen-image">
+</body>
 </html>
 ''', 404
 
 @app.route('/')
 @app.route('/index')
 def index():
-    style = url_for("static", filename = "lab1.css")
+    style = url_for("static", filename="lab1.css")
     return '''<!doctype html>
         <html>
         <head>
-            <link rel = "stylesheet" href="''' + style +'''"
+            <link rel="stylesheet" href="''' + style + '''">
             <title>НГТУ, ФБ, Лабораторные работы</title>
         </head>
         <header>
@@ -68,7 +107,7 @@ def index():
         <footer>Токарский Илья Андреевич, ФБИ-22, 3 курс, 2024</footer>
         </html>''', 200
 
-
+# Другие маршруты и обработчики ошибок
 @app.route('/error/400')
 def error_400():
     return 'Bad Request', 400
@@ -115,13 +154,13 @@ def internal_error(error):
 
 @app.route('/model')
 def heavy_metal():
-    path = url_for("static", filename = "Bella.jpg")
-    style = url_for("static", filename = "lab1.css")
+    path = url_for("static", filename="Bella.jpg")
+    style = url_for("static", filename="lab1.css")
     return '''
 <!doctype html>
 <html>
     <head>
-        <link rel = "stylesheet" href="''' + style +'''"
+        <link rel="stylesheet" href="''' + style + '''">
         <title>BELLA HADID</title>
         <style>
             body {
@@ -144,6 +183,7 @@ def heavy_metal():
     'X-Custom-Header-2': 'bella'
 }
 
-
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()  # Создание таблиц в базе данных
     app.run(debug=True)
